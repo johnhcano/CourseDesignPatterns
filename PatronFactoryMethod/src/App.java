be35fe900c71e309925598a0c0538399361746a2
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-01 20:36:16 -0500

    2120

--- PatronFactoryMethod/src/App.java
+++ PatronFactoryMethod/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Button buttonWindows = new WindowsButton();
+        Button buttonHTML = new HTMLButton();
+
+        Dialog dialogWindows = new WindowsDialog();
+        Dialog dialogWeb = new WebDialog();
+
+        buttonWindows = dialogWindows.createButton();
+        buttonWindows.onClick();
+        buttonWindows.render();
+
+        buttonHTML = dialogWeb.createButton();
+        buttonHTML.onClick();
+        buttonHTML.render();
+
     }
 }

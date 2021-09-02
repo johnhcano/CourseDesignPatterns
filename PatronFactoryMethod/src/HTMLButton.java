2c530131fc0072651c1f6ddb1256841a0ece057f
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-01 20:02:52 -0500

    2120

---
+++ PatronFactoryMethod/src/HTMLButton.java
@@ -0,0 +1,10 @@
+public class HTMLButton implements Button {
+    
+    public void render(){
+        System.out.println("Button Render HTML");
+    }
+
+    public void onClick(){
+        System.out.println("Button onClick HTML");
+    }
+}

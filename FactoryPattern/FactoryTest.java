3ad6de412f14fca1e8cd913b9a7257e382319c3f
JhonHCanoB <jhon.canob@campusucc.edu.co>
2024-07-08 04:17:29 +0000

    Factory Pattern

--- FactoryPattern/FactoryTest.java
+++ FactoryPattern/FactoryTest.java
@@ -1,5 +1,7 @@
 public class FactoryTest {
+    
     public static void main (String[] args){
+        
         TelefonoInteligente factory = new TelefonoInteligente();
         Sony sony = (Sony)factory.createTelefonoInteligente("Sony");
         sony.bateria();

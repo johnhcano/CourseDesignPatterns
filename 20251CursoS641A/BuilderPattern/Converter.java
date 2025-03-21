849e84cc8f5332e23cf72635d16a53986ef8b074
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-03-21 12:56:34 +0000

    Creación de la clase Converter

---
+++ 20251CursoS641A/BuilderPattern/Converter.java
@@ -0,0 +1,25 @@
+public class Converter {
+
+    private String document;
+
+    public Converter(String document){
+        this.document = document;
+    }
+
+    public void makeLine(){
+        System.out.println();
+    }
+
+    public void makeParagraph(){
+        System.out.println();
+    }
+
+    public void makeTable(){
+        System.out.println();
+    }
+
+    public String getDocument(){
+        return document;
+    }
+
+}

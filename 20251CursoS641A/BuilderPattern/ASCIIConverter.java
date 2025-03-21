95e0cabce845ce70f43ddbce1ca72f6d0e2a6768
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-03-21 13:16:14 +0000

    Creación de la clase ASCIIConverter

---
+++ 20251CursoS641A/BuilderPattern/ASCIIConverter.java
@@ -0,0 +1,19 @@
+public class ASCIIConverter extends Converter {
+
+
+    public ASCIIConverter(String document){
+        super(document);
+    }
+
+    public void makeLine(){
+        System.out.println("Línea con ASCIIConverter");
+    }
+
+    public void makeParagraph(){
+        System.out.println("Párrafo con ASCIIConverter");
+    }
+
+    public void makeTable(){
+        System.out.println("Tabla con ASCIIConverter");
+    }
+}
